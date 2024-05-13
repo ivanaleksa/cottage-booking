e37d5c7ee60ac270df33d53e5f67245cb33bcdf4
Ivan <ivan.aleksandrov03@mail.ru>
2024-05-13 10:13:10 +0400

    add a method for cottage booking

--- index.php
+++ index.php
@@ -52,6 +52,25 @@ else if ($_SERVER['REQUEST_METHOD'] === 'GET' && $path === '/get_bookings') {
     $stmt = $pdo->query("SELECT * FROM cottage_booking");
     echo json_encode($stmt->fetchAll());
 }
+else if ($_SERVER['REQUEST_METHOD'] === 'POST' && $path === '/add_booking') {
+    if (isset($_POST['cottageId']) && isset($_POST['name']) && isset($_POST['phoneNumber']) && isset($_POST['startDate']) && isset($_POST['endDate'])) {
+        $stmp = $pdo->prepare("INSERT INTO cottage_booking (cottage_id, client_name, client_phone_number, booking_start_at, booking_end_at)
+                                    VALUES (:cottage_id, :client_name, :client_phone_number, :booking_start_at, :booking_end_at)");
+        
+        $stmp->bindParam(':cottage_id', $_POST['cottageId']);
+        $stmp->bindParam(':client_name', $_POST['name']);
+        $stmp->bindParam(':client_phone_number', $_POST['phoneNumber']);
+        $stmp->bindParam(':booking_start_at', $_POST['startDate']);
+        $stmp->bindParam(':booking_end_at', $_POST['endDate']);
+        
+        $stmp->execute();
+        echo json_encode(array('status' => 'ok'));
+    }
+    else {
+        http_response_code(400);
+        echo json_encode(array('error' => 'Unprocessable entity'));
+    }
+}
 else {
     http_response_code(404);
     echo json_encode(array('error' => 'Page not found'));
